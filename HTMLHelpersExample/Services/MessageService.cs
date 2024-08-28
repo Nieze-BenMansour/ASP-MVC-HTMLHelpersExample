62100146094e3b3d09b960b5321b31ccc2b358b6
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-08-28 09:41:17 +0100

    Add project files.

---
+++ HTMLHelpersExample/Services/MessageService.cs
@@ -0,0 +1,12 @@
+﻿namespace HTMLHelpersExample.Services
+{
+    public class MessageService
+    {
+        public string GetMessage()
+        {
+            // Ici, vous pouvez implémenter la logique pour générer un message dynamique
+            // Pour cet exemple, nous allons simplement retourner une chaîne statique
+            return "Bonjour, monde !";
+        }
+    }
+}
